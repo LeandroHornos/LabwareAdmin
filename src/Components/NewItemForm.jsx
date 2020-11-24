672bae0c71088d8db316606d54290e824756cdfa
Lean <leandrohornos@gmail.com>
2020-11-23 21:36:33 -0300

    Creo nuevo item desde schema

--- src/Components/NewItemForm.jsx
+++ src/Components/NewItemForm.jsx
@@ -1,3 +1,19 @@
+/* 
+NewItemForm:
+************ 
+Este es el formulario para la creación de un nuevo item. 
+Durante el proceso se introduce la información que describe al objeto de
+forma general. Aquí no se especifican cantidades, ni ubicaciones, ni estados.
+Es por ello que el inventario puede describir items aunque no se tengan
+físicamente.
+
+Los items se organizan en categorías y subcategorías. Si las mismas no existen,
+se pueden crear dándole click a un buton "nueva", el cual cambia el select por un 
+input para que se pueda crear la categoría, además de mostrar un input para crear una
+subcategoría. Todas las categorías deben tener al menos una subcategoría. Queda 
+a criterio del usuario como clasificar las categorías de los items.
+*/
+
 import React, { useState, useContext } from "react";
 
 import Button from "react-bootstrap/Button";
@@ -7,11 +23,13 @@ import { FormControl, FormGroup } from "react-bootstrap";
 /* Firebase */
 import firebaseApp from "../firebaseApp";
 import { AuthContext } from "../Auth";
+import ItemSchema from "../Models/ItemSchema";
 
 // Router
 import { useHistory } from "react-router-dom";
 
 import AccordionFormWrap from "./AccordionFormWrap.jsx";
+import InventorySchema from "../Models/InventorySchema";
 
 /* New Item form ------------------------------------ */
 
@@ -25,20 +43,12 @@ const NewItemForm = (props) => {
   const [subcatlist, setSubcatlist] = useState([]);
   const [category, setCategory] = useState("");
   const [subcategory, setSubcategory] = useState("");
-  const [location, setLocation] = useState("");
-  const [sublocation, setSublocation] = useState("");
-  const [sublocationlist, setSublocationList] = useState([]);
-  const [newlocation, setNewLocation] = useState(false);
-  const [newsublocation, setNewSublocation] = useState(false);
-  const [ammount, setAmmount] = useState(0);
-  const [status, setStatus] = useState("");
-  const [newstatus, setNewStatus] = useState(false);
-  const [groupname, setGroupName] = useState("");
 
   const categories = props.inventory.categories.map((cat) => cat.name);
-  const locations = props.inventory.locations.map((loc) => loc.name);
 
   const listSubcats = (name, catArray) => {
+    /* Lista las subcategorías correspondientes 
+    cada vez que se selecciona una categoría */
     let subcats = [];
     catArray.forEach((cat) => {
       if (cat.name === name) {
@@ -48,20 +58,12 @@ const NewItemForm = (props) => {
     return subcats;
   };
 
-  const listSublocations = (name, locArray) => {
-    let sublocs = [];
-    locArray.forEach((loc) => {
-      if (loc.name === name) {
-        sublocs = loc.sublocations;
-      }
-    });
-    return sublocs;
-  };
-
   const updateInventory = () => {
+    /* Determina si se han creado nuevas categorías y/o subcategorías.
+    De ser así, actualiza la lista de categorías en el inventario al que
+    corresponde el item */
     let inventory = props.inventory;
-    const hasChanged =
-      newcat || newsubcat || newlocation || newsublocation || newstatus;
+    const hasChanged = newcat || newsubcat;
     if (newcat) {
       inventory.categories.push({
         name: category,
@@ -78,29 +80,18 @@ const NewItemForm = (props) => {
         }
       });
     }
-    if (newlocation) {
-      inventory.locations.push({ name: location, sublocations: [sublocation] });
-    }
-    if (!newlocation && newsublocation) {
-      let index = 0;
-      inventory.locations.forEach((loc) => {
-        if (loc.name === location) {
-          inventory.locations[index].sublocations.push(sublocation);
-        } else {
-          index++;
-        }
-      });
-    }
-    if (newstatus) {
-      inventory.statuses.push(status);
-    }
     return { hasChanged, newInventory: inventory };
   };
 
   const handleCreateItem = async () => {
+    /* Maneja la accion que se dispara cuando el
+    usuario le da al botón de crear item. Es la 
+    encargada de estructurar la data en base al schema y
+    comunicarse con la base de datos. */
     const db = firebaseApp.firestore();
 
     let data = {
+      ...InventorySchema,
       inventoryId: props.inventory.id,
       creationdate: new Date(),
       creatorId: currentUser.uid,
@@ -108,16 +99,6 @@ const NewItemForm = (props) => {
       description,
       category,
       subcategory,
-      groups: [
-        {
-          date: new Date(),
-          groupname,
-          location,
-          sublocation,
-          status,
-          ammount,
-        },
-      ],
     };
     data = {
       ...data,
@@ -292,183 +273,6 @@ const NewItemForm = (props) => {
             {newsubcat ? "Cancelar" : "Nueva"}
           </Button>
         )}
-        {/* --- LOCATION ------------------------------------------------------- */}
-        <FormGroup>
-          <Form.Label>Location: </Form.Label>
-          {/* SELECT LOCATION */}
-          {!newlocation && (
-            <Form.Control
-              as="select"
-              value={location}
-              onChange={(e) => {
-                setLocation(e.target.value);
-                setSublocation("");
-                setSublocationList(
-                  listSublocations(e.target.value, props.inventory.locations)
-                );
-              }}
-            >
-              <option value={""}>Elije una ubicación</option>
-              {locations.map((cat) => {
-                return (
-                  <option key={cat.index + "-" + cat} value={cat}>
-                    {cat}
-                  </option>
-                );
-              })}
-            </Form.Control>
-          )}
-          {/* NEW LOCATION INPUT */}
-          {newlocation && (
-            <FormControl
-              placeholder="Ej: Armario C"
-              style={{ marginTop: "10px" }}
-              type="text"
-              onChange={(e) => {
-                setLocation(e.target.value);
-              }}
-            ></FormControl>
-          )}
-        </FormGroup>
-        {/* TOGGLE NEW LOCATION */}
-        <Button
-          size="sm"
-          style={{ marginBottom: "10px" }}
-          variant={newlocation ? "warning" : "info"}
-          onClick={(e) => {
-            e.preventDefault();
-            newlocation ? setNewLocation(false) : setNewLocation(true);
-          }}
-        >
-          {newlocation ? "Cancelar" : "Nueva"}
-        </Button>
-        {/* SUBLOCATION */}
-        <FormGroup>
-          <Form.Label>Sub-ubicación: </Form.Label>
-          {/* SUBLOCATION SELECT */}
-          {!newlocation && !newsublocation && (
-            <Form.Control
-              as="select"
-              value={sublocation}
-              onChange={(e) => {
-                setSublocation(e.target.value);
-              }}
-            >
-              <option value={""}>Elije una sub-ubicacion</option>
-              {sublocationlist.map((subloc) => {
-                return (
-                  <option key={subloc.index + "-" + subloc} value={subloc}>
-                    {subloc}
-                  </option>
-                );
-              })}
-            </Form.Control>
-          )}
-          {/* SUBLOCATION INPUT */}
-          {(newlocation || newsublocation) && (
-            <FormControl
-              placeholder="Ej: Estante 3"
-              value={sublocation}
-              style={{ marginTop: "10px" }}
-              type="text"
-              onChange={(e) => {
-                setSublocation(e.target.value);
-              }}
-            ></FormControl>
-          )}
-        </FormGroup>
-        {!newlocation && (
-          <Button
-            size="sm"
-            style={{ marginBottom: "10px" }}
-            variant={newsublocation ? "warning" : "info"}
-            onClick={(e) => {
-              e.preventDefault();
-              newsublocation
-                ? setNewSublocation(false)
-                : setNewSublocation(true);
-            }}
-          >
-            {newsublocation ? "Cancelar" : "Nueva"}
-          </Button>
-        )}
-        {/* ---------------- FIN LOCATION  ----------- */}
-        <FormGroup>
-          {/* STATUS */}
-
-          <Form.Label>Estado: </Form.Label>
-
-          {/* STATUS SELECT */}
-          {!newstatus && (
-            <Form.Control
-              as="select"
-              value={status}
-              onChange={(e) => {
-                setStatus(e.target.value);
-              }}
-            >
-              <option value={""}>Elije un estado</option>
-              {props.inventory.statuses.map((status) => {
-                return (
-                  <option key={status} value={status}>
-                    {status}
-                  </option>
-                );
-              })}
-            </Form.Control>
-          )}
-
-          {/* STATUS INPUT */}
-          {newstatus && (
-            <FormControl
-              placeholder="Ej: En uso"
-              value={status}
-              style={{ marginTop: "10px" }}
-              type="text"
-              onChange={(e) => {
-                setStatus(e.target.value);
-              }}
-            ></FormControl>
-          )}
-
-          {/* STATUS BOTON NUEVA OPCION */}
-          <Button
-            size="sm"
-            style={{ margin: "10px 0px" }}
-            variant={newstatus ? "warning" : "info"}
-            onClick={(e) => {
-              e.preventDefault();
-              newstatus ? setNewStatus(false) : setNewStatus(true);
-            }}
-          >
-            {newstatus ? "Cancelar" : "Nuevo"}
-          </Button>
-        </FormGroup>
-        <FormGroup>
-          <Form.Label>Cantidad: </Form.Label>
-          <FormControl
-            type="number"
-            value={ammount}
-            onChange={(e) => {
-              setAmmount(e.target.value);
-            }}
-          ></FormControl>
-        </FormGroup>
-        <FormGroup>
-          <Form.Label>Grupo: </Form.Label>
-          <FormControl
-            placeholder="Ej: Guardados en depósito"
-            type="text"
-            onChange={(e) => {
-              setGroupName(e.target.value);
-            }}
-          ></FormControl>
-          <Form.Text className="text-muted">
-            Puedes crear distintos grupos dentro de un mismo item, por ejemplo
-            para distribuir el stock entre diferentes marcas, ubicaciones o
-            estados
-          </Form.Text>
-        </FormGroup>
         <Button variant="info" block onClick={() => handleCreateItem()}>
           Agregar item
         </Button>

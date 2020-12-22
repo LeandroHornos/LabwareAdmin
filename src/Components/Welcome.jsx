6d1e68c563f25c59c65114fe53453f6433850e32
Lean <leandrohornos@gmail.com>
2020-12-22 01:53:42 -0300

    cambio colores en welcome y panel lateral

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -7,45 +7,57 @@ import "./Welcome.css";
 const Welcome = () => {
   const history = useHistory();
   return (
-    <div
-      className="row welcome"
-      style={{ backgroundImage: "url(./img/wavecut.png)" }}
-    >
+    <div className="row welcome">
       <div className="col-md-6 col-lg-7"></div>
       <div
-        className="col-md-5 col-lg-4 d-flex flex-column aling-items-center justify-content-center"
-        style={{ minHeight: "100vh", padding: "10px" }}
+        className="col-md-5 col-lg-4"
+        style={{
+          minHeight: "100vh",
+          padding: "10px",
+        }}
       >
-        <div style={{ textAlign: "center" }} className="welcome-box boxshadow">
-          <img
-            src="./img/carbo-logo-2-rojo.svg"
-            alt="logo"
-            style={style.logo}
-          />
-          <p>Te damos la bienvenida a</p>
-          <h1 style={{ color: "rgb(0,160,60)" }}>LabwareAdmin</h1>
-          <p style={{ fontStyle: "italic" }}>By Carbo</p>
-          <p>Lleva el inventario de tu laboratorio!</p>
-          <Button
-            className="boxshadow"
-            variant="success"
-            onClick={(e) => {
-              history.push("/signin");
-            }}
-            block
-          >
-            Ingresar
-          </Button>
-          <Button
-            className="boxshadow"
-            variant="info"
-            onClick={(e) => {
-              history.push("/signup");
-            }}
-            block
-          >
-            Registrate
-          </Button>
+        <div
+          style={{
+            textAlign: "center",
+            backgroundImage: "url(./img/wavecut.png)",
+          }}
+          className="welcome-box boxshadow d-flex flex-column aling-items-center justify-content-around"
+        >
+          <div>
+            <img
+              src="./img/carbo-logo-2-rojo.svg"
+              alt="logo"
+              style={style.logo}
+            />
+            <p style={{ fontStyle: "italic", margin: "0px" }}>Carbo</p>
+            <h1 style={{ color: "rgb(150, 230, 170)", padding: "0px" }}>
+              LabwareAdmin
+            </h1>
+          </div>
+
+          <div>
+            {" "}
+            <Button
+              className="boxshadow"
+              variant="success"
+              onClick={(e) => {
+                history.push("/signin");
+              }}
+              block
+            >
+              Ingresar
+            </Button>
+            <Button
+              className="boxshadow"
+              variant="info"
+              onClick={(e) => {
+                history.push("/signup");
+              }}
+              block
+            >
+              Registrate
+            </Button>
+          </div>
         </div>
       </div>
       <div className="col-lg-1"></div>
